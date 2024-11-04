4bc0cfe889deb51b6e4e1fb5daba31cfd8a60acc
Afia Rahman <rahmanmou26@gmail.com>
2024-11-04 16:27:30 +0600

    added common menu card component

--- src/components/PopularMenu.jsx
+++ src/components/PopularMenu.jsx
@@ -1,9 +1,42 @@
 import TitleLine from "./shared/TitleLine";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/bundle";
+import { Autoplay, Navigation } from 'swiper/modules';
+import burgerImg from "../assets/burger.jfif"
+import pizzaImg from "../assets/pizza.png"
+import friesImg from "../assets/fries.jfif"
+import nuggetImg from "../assets/nugget.jfif"
+import MenuCard from "./cards/MenuCard";
 
 const PopularMenu = () => {
     return (
         <div className='bg-[#FBF7F2] py-7 lg:py-20 px-7 lg:px-28'>
-            <TitleLine title={'POPULAR FOOD ITEMS'} sub={'Crispy, Every Bite Taste'}/>
+            <TitleLine title={'POPULAR FOOD ITEMS'} sub={'Crispy, Every Bite Taste'} />
+
+            <Swiper slidesPerView={4}
+                spaceBetween={30}
+                navigation={true} modules={[Autoplay, Navigation]}
+                autoplay={{ delay: 2500, disableOnInteraction: false, }} loop={true} className="mySwiper mt-6 lg:mt-12">
+                <SwiperSlide>
+                    <MenuCard img={burgerImg} title={'vegetables burger'} desc={'Barbecue Italian cuisine pizza'}/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MenuCard img={pizzaImg} title={'Spacial Pizza'} desc={'Barbecue Italian cuisine pizza'}/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MenuCard img={friesImg} title={'Spacial French fries'} desc={'Barbecue Italian cuisine'}/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MenuCard img={nuggetImg} title={'Cuisine Chicken'} desc={'Japanese Cuisine Chicken'}/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MenuCard img={burgerImg} title={'vegetables burger'} desc={'Barbecue Italian cuisine pizza'}/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MenuCard img={nuggetImg} title={'Cuisine Chicken'} desc={'Japanese Cuisine Chicken'}/>
+                </SwiperSlide>                
+            </Swiper>
         </div>
     );
 };
---
+++ src/components/cards/MenuCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MenuCard = ({img, title, desc}) => {
+    return (
+        <div className="card bg-base-100 w-64 h-72">
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Burger" className="rounded-xl h-28" />
+            </figure>
+            <hr className="border-2 border-[#BD1F17] w-20 mx-auto rounded-xl my-3" />
+            <div className="card-body items-center text-center pt-0">
+                <h2 className="capitalize font-bebas text-2xl mt-5">{title}</h2>
+                <p className="font-inter text-sm">{desc}</p>
+            </div>
+        </div>
+    );
+};
+
+export default MenuCard;
