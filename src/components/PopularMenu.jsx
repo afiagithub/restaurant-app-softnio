aca2b71881ffeb425bbf530af49a382c9e23b6b6
Afia Rahman <rahmanmou26@gmail.com>
2024-11-04 16:45:45 +0600

    added popular menu items section

--- src/components/PopularMenu.jsx
+++ src/components/PopularMenu.jsx
@@ -8,34 +8,47 @@ import pizzaImg from "../assets/pizza.png"
 import friesImg from "../assets/fries.jfif"
 import nuggetImg from "../assets/nugget.jfif"
 import MenuCard from "./cards/MenuCard";
+import sideImg from "../assets/menuside.png"
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 const PopularMenu = () => {
     return (
-        <div className='bg-[#FBF7F2] py-7 lg:py-20 px-7 lg:px-28'>
-            <TitleLine title={'POPULAR FOOD ITEMS'} sub={'Crispy, Every Bite Taste'} />
+        <div className='bg-[#FBF7F2] py-7 lg:py-20 px-7 lg:px-28 relative'>
+            <img src={sideImg} alt="" className="hidden lg:flex absolute left-0 bottom-36 w-32" />
+            <div className="flex flex-row justify-between items-center">
+                <TitleLine title={'POPULAR FOOD ITEMS'} sub={'Crispy, Every Bite Taste'} />
+
+                <div className="flex flex-row gap-5">
+                    <button className="arrow-left arrow w-10 h-10 bg-white rounded-full shadow-lg text-lg 
+                    hover:text-[#BD1F17]"><IoIosArrowBack className="mx-auto" /></button>
+                    <button className="arrow-right arrow w-10 h-10 bg-white rounded-full shadow-lg text-lg 
+                    hover:text-[#BD1F17]"><IoIosArrowForward className="mx-auto" /></button>
+                </div>
+            </div>
 
             <Swiper slidesPerView={4}
                 spaceBetween={30}
-                navigation={true} modules={[Autoplay, Navigation]}
+                navigation={{ nextEl: ".arrow-right", prevEl: ".arrow-left" }} modules={[Autoplay, Navigation]}
                 autoplay={{ delay: 2500, disableOnInteraction: false, }} loop={true} className="mySwiper mt-6 lg:mt-12">
                 <SwiperSlide>
-                    <MenuCard img={burgerImg} title={'vegetables burger'} desc={'Barbecue Italian cuisine pizza'}/>
+                    <MenuCard img={burgerImg} title={'vegetables burger'} desc={'Barbecue Italian cuisine pizza'} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <MenuCard img={pizzaImg} title={'Spacial Pizza'} desc={'Barbecue Italian cuisine pizza'}/>
+                    <MenuCard img={pizzaImg} title={'Spacial Pizza'} desc={'Barbecue Italian cuisine pizza'} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <MenuCard img={friesImg} title={'Spacial French fries'} desc={'Barbecue Italian cuisine'}/>
+                    <MenuCard img={friesImg} title={'Spacial French fries'} desc={'Barbecue Italian cuisine'} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <MenuCard img={nuggetImg} title={'Cuisine Chicken'} desc={'Japanese Cuisine Chicken'}/>
+                    <MenuCard img={nuggetImg} title={'Cuisine Chicken'} desc={'Japanese Cuisine Chicken'} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <MenuCard img={burgerImg} title={'vegetables burger'} desc={'Barbecue Italian cuisine pizza'}/>
+                    <MenuCard img={burgerImg} title={'vegetables burger'} desc={'Barbecue Italian cuisine pizza'} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <MenuCard img={nuggetImg} title={'Cuisine Chicken'} desc={'Japanese Cuisine Chicken'}/>
-                </SwiperSlide>                
+                    <MenuCard img={nuggetImg} title={'Cuisine Chicken'} desc={'Japanese Cuisine Chicken'} />
+                </SwiperSlide>
             </Swiper>
         </div>
     );
